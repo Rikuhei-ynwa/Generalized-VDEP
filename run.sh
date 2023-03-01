35e8cabfb77c4e022ad51f6b1a35a1a6d4696c34
Rikuhei-ynwa <umemoto.rikuhei.ynwa@gmail.com>
2023-03-01 11:23:48 +0900

    add to analyze FIFA World Cup 2022

--- run.sh
+++ run.sh
@@ -4,9 +4,14 @@
 data360=(euro2020 euro2022)
 # training with uefa statsbomb data
 for info in ${data360[@]};do
-        python train_GVDEP_opendata.py --data statsbomb --game ${info} --n_nearest 11 --no_games -1 --predict_actions --test --teamView England --skip_load_rawdata --skip_preprocess --skip_train
+        python train_GVDEP_opendata.py --data statsbomb --game ${info} --n_nearest 11 --no_games -1 # --test --teamView England --skip_load_rawdata --skip_preprocess --skip_train
 done
 # Storing, calculating and showing F1-scores by each "n_nearest" and "CV".
 for info in ${data360[@]};do
-        python calc_f1scores.py --game ${info} --no_games -1 --predict_actions --calculate_f1scores --show_f1scores
+        python calc_f1scores.py --game ${info} --no_games -1 --calculate_f1scores --show_f1scores
 done
+
+
+# FIFA World Cup
+python train_GVDEP_opendata.py --data statsbomb --game wc2022 --n_nearest 11 --no_games -1 # --test --teamView Argentina --skip_load_rawdata --skip_preprocess --skip_train
+python calc_f1scores.py --game wc2022 --no_games -1 --calculate_f1scores --show_f1scores
