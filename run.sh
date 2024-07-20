880e6befcf246e2c4ba59848367b7f8b9220adff
Rikuhei-ynwa <umemoto.rikuhei@g.sp.m.is.nagoya-u.ac.jp>
2024-07-20 18:57:10 +0900

    Minor fixes

--- run.sh
+++ run.sh
@@ -8,7 +8,7 @@ $HOME/.local/bin/poetry install
 # GVDEP 
 # UEFA Euro 2020
 poetry run python main_evaluate.py --data statsbomb --game euro2020 --n_nearest 11 --no_games -1 --date_opendata 20240702 --date_experiment ${date_experiment} --model xgboost --test --teamView England
-poetry run python main_verify.py --data statsbomb --game euro2020 --date_opendata 20230702 --date_experiment ${date_experiment} --model xgboost --k_fold 5
+poetry run python main_verify.py --data statsbomb --game euro2020 --date_opendata 20240702 --date_experiment ${date_experiment} --model xgboost --k_fold 5
 
 
 # UEFA Women's Euro 2022 (TBD)
